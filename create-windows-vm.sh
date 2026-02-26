3eeec1ab71f550d3ad91190cc93c340166e4ba65
Your Name <you@example.com>
2026-02-26 11:23:34 +0800

    fix: 修正音频设备配置，将 audiodev 属性移动到支持的 hda-duplex 设备

--- create-windows-vm.sh
+++ create-windows-vm.sh
@@ -187,8 +187,8 @@ qemu-system-x86_64 \
   \
   # 音频配置 - 在没有音频设备的系统上使用空音频设备避免 ALSA 错误
   -audiodev none,id=audio0 \
-  -device intel-hda,id=sound0,bus=pcie.0,addr=0x3,audiodev=audio0 \
-  -device hda-duplex,id=sound0-codec0,bus=sound0.0,cad=0 \
+  -device intel-hda,id=sound0,bus=pcie.0,addr=0x3 \
+  -device hda-duplex,id=sound0-codec0,bus=sound0.0,cad=0,audiodev=audio0 \
   \
   -device virtio-vga,xres=1920,yres=1080,id=video0,bus=pcie.0,addr=0x2 \
   -vnc :0 \
