d0ebc8d0302f8c5bd9e87668f3ed957733e4044d
Your Name <you@example.com>
2026-02-26 18:12:26 +0800

    修复 QEMU 运行时警告 - 修正设备类名

--- create-windows-vm.sh
+++ create-windows-vm.sh
@@ -196,8 +196,8 @@ qemu-system-x86_64 \
   -rtc base=localtime,clock=host \
   -no-hpet \
   -global kvm-pit.lost_tick_policy=discard \
-  -global ICH9-PM.disable_s3=1 \
-  -global ICH9-PM.disable_s4=1 \
+  -global ich9-pm.disable_s3=1 \
+  -global ich9-pm.disable_s4=1 \
   -device ich9-intel-hda,addr=0x1b,multifunction=on \
   -device virtio-balloon-pci,id=balloon0,bus=pcie.0,addr=0x7,disable-legacy=on \
   -nodefaults \
